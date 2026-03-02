3e3a7cacc22b77968ac16e57019265578fd71b98
Rio0294 <datey.riya@gmail.com>
2026-03-02 16:01:10 -0600

    Added silver layer

---
+++ functions.py
@@ -0,0 +1,18 @@
+
+# def make_coffee():
+#     print("Start machine")
+#     print("Make coffee")
+#     print("Add milk")
+#     print("enjoy it")
+
+# print("Wake up")
+# make_coffee()
+# print("Working for a while")
+# make_coffee()
+
+
+
+def clean_name(name):
+    print(name.strip().lower())
+
+clean_name(" Riya ")
